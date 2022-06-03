4116c9e60d7a855e73ae52ce9eb8d8ed63d2fff6
rojas1995 <jrojasgranado@gmail.com>
2022-06-03 03:05:17 -0700

    add geolocation to the api request

--- twitter_app/api/views.py
+++ twitter_app/api/views.py
@@ -8,8 +8,8 @@ class GetAPIData(TemplateView):
 
     def get_context_data(self, **kwargs):
         '''quey for twitter'''
-        fields = 'created_at,public_metrics,attachments,entities,geo,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source,withheld'
-        query_params = {'query': 'elden', 'tweet.fields': fields, 'max_results': 10}
+        fields = 'created_at,public_metrics,attachments,entities,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source,withheld'
+        query_params = {'query': 'spain', 'expansions': 'author_id,in_reply_to_user_id,geo.place_id', 'tweet.fields': fields, 'place.fields': 'contained_within,country,country_code,full_name,geo,id,name,place_type', 'max_results': 50}
         context = {
             'data': get_data(query_params),
         }
